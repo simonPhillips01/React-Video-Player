e47175d35628cce0814e803ca76addd5aa682e8b
Simon Phillips <simon.phillips01@icloud.com>
2019-10-24 14:28:01 +0100

    Scaffolding the app

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import StyledPlaylistItem from './styles/StyledPlaylistItem';
 
-const PlaylistItem = ({ video, active, played }) => (
-  <StyledPlaylistItem active={active} played={played}>
-    <div className="wbn-player__video-nr">{video.num}</div>
-    <div className="wbn-player__video-name">{video.title}</div>
-    <div className="wbn-player__video-time">{video.duration}</div>
-  </StyledPlaylistItem>
-);
+const PlaylistItem = props => (
+  <div>
+    PlaylistItem!!
+  </div>
+)
 
-export default PlaylistItem;
+export default PlaylistItem;  
+
+// import React from 'react';
+// import StyledPlaylistItem from './styles/StyledPlaylistItem';
+
+// const PlaylistItem = ({ video, active, played }) => (
+//   <StyledPlaylistItem active={active} played={played}>
+//     <div className="wbn-player__video-nr">{video.num}</div>
+//     <div className="wbn-player__video-name">{video.title}</div>
+//     <div className="wbn-player__video-time">{video.duration}</div>
+//   </StyledPlaylistItem>
+// );
+
+// export default PlaylistItem;
